8afff541996211c7884505fba1b2e35d2c43bebe
Stephen <stephen.singer.098@gmail.com>
2020-08-22 20:14:22 +0800

    Updates

---
+++ precache-manifest.b6170dee63a9e6947817c9b7cd11e934.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9beecc7c1fefb2680785fc8af80b1d2c",
+    "url": "/online-portfolio/index.html"
+  },
+  {
+    "revision": "114a695292659244283c",
+    "url": "/online-portfolio/static/js/2.8206f3a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/online-portfolio/static/js/2.8206f3a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0af12d2c032aa7f304d",
+    "url": "/online-portfolio/static/js/main.4a91b329.chunk.js"
+  },
+  {
+    "revision": "f213551161415a8a90d2",
+    "url": "/online-portfolio/static/js/runtime-main.9a084aee.js"
+  }
+]);
